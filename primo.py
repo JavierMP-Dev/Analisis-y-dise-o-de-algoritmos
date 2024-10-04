bd68f951bccfffb3da0ecbbed6200e8b2be2a52d
Javier <montoya.javier.15@hotmail.com>
2024-10-04 00:04:40 -0600

    comentarios

--- primo.py
+++ primo.py
@@ -2,17 +2,18 @@ print("Números primos")
 
 limite = 104750
 contador_primos=0
-def es_primo(n):
-    if n <= 1:
+
+def es_primo(n): #funcion para identificar un numero primo
+    if n <= 1: #revisando que sea mayor o igual a 1
         return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):#se comprueba si es divisible entre 2
+        if n % i == 0: #se comprueba el residu
             return False
-    return True
+    return True #retorna los numeros primos
 
 for elemento in range(2, limite + 1):
-    if es_primo(elemento):
-        contador_primos=contador_primos+1
+    if es_primo(elemento): #si es primo se imprime 
+        contador_primos=contador_primos+1 #llevando la cuenta de numeros primos
         print(elemento)
 
 print("Total de primos: " + str(contador_primos))
