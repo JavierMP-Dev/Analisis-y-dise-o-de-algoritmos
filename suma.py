f11c627c1985b5d9b57f019be41c31348fb341ce
Javier <montoya.javier.15@hotmail.com>
2024-11-12 14:58:11 -0600

    operadores basicos con funciones

--- suma.py
+++ suma.py
@@ -1,12 +1,25 @@
 
 def suma(num1, num2):
     suma = num1 + num2
-    print("La suma es= ",suma)
-    return suma
+    print("Suma: ", num1, "+", num2, "=",suma)
+    
+def resta(num1, num2):
+    resta = num1 - num2
+    print("Resta:", num1, "-", num2, "=", resta)
 
+def multi(num1, num2):
+    multi = num1 * num2
+    print("Multiplicacion: ", num1, "*", num2, "=", multi)
+
+def divi(num1, num2):
+    divi = num1 / num2
+    print("Division: ", num1, "/", num2, "=", divi)
 
 print("Programa de funciones")
 num1 = int(input("Num1-->"))
 num2 = int(input("Num2-->"))
 
 suma(num1, num2)
+resta(num1, num2)
+divi(num1, num2)
+multi(num1, num2)
